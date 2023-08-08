6d966ed96297b7e8c7de6a3be30bdefa148ef7cf
Nobuyoshi Namba <ramennobu0421@gmail.com>
2023-08-08 11:19:22 +0900

    XMLファイル読み書き機能実装

---
+++ FileReadWrite/Domains/XmlSampleObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FileReadWrite.Domains
+{
+    public class XmlSampleObjectAccessor
+    {
+        private static readonly string filePath = System.Environment.CurrentDirectory + @"\xmlSample.xml";
+
+        private static readonly XmlFileReader xmlReader = new XmlFileReader();
+        private static readonly XmlFileWriter xmlWriter = new XmlFileWriter();
+
+
+        public static XmlSampleObject GetSample()
+        {
+            return xmlReader.Read<XmlSampleObject>(filePath);
+        }
+
+        public static void SetSample(XmlSampleObject sampleObject)
+        {
+            xmlWriter.Write<XmlSampleObject>(filePath, sampleObject);
+        }
+
+        // @@TEST
+        public static void InitSample()
+        {
+
+            Console.WriteLine(filePath);
+            var sampleObject = new XmlSampleObject();
+
+            sampleObject.books = new List<SampleBook>
+            {
+                new SampleBook{ id = 1, title = "sample_novel", publisher = "sample_syotenn"},
+                new SampleBook{ id = 2, title = "sample_zasshi", publisher = "sample_syotenn"},
+                new SampleBook{ id = 3, title = "sample_novel", publisher = "sample_syotenn"}
+            };
+
+            xmlWriter.Write<XmlSampleObject>(filePath, sampleObject);
+        }
+    }
+
+    public class XmlSampleObject
+    {
+        [XmlArray("list_book")]
+        [XmlArrayItem("book")]
+        public List<SampleBook> books {  get; set; }
+    }
+
+    public class SampleBook
+    {
+        [XmlAttribute("id")]
+        public int id { get; set; }
+        [XmlElement("title")]
+        public string title { get; set; }
+        [XmlElement("publisher")]
+        public string publisher { get; set; }
+    }
+}
--- FileReadWrite/Program.cs
+++ FileReadWrite/Program.cs
@@ -1,5 +1,43 @@
 ﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+
+using FileReadWrite.Domains;
+using System.Runtime.CompilerServices;
+
+// XMLReadWrite_Test
+try
+{
+    // @@TEST : 初期化
+    //XmlSampleObjectAccessor.InitSample();
+
+    //ファイルを読み取り、表示。
+    Console.WriteLine("---------- 読み取り開始 ----------");
+    var xmlSample = XmlSampleObjectAccessor.GetSample();
+    if ( xmlSample != null)
+    {
+        foreach(var sample in xmlSample.books)
+        {
+            Console.WriteLine("id :" + sample.id + ",title : " + sample.title + ", publisher : " + sample.publisher);
+        }
+    }
+    Console.WriteLine("---------- 読み取り終了 ----------");
+
+
+    // 中身を編集して、書き込む
+    xmlSample.books[0].title = "編集後タイトル";
+
+    Console.WriteLine("---------- 編集後読み取り開始 ----------");
+    if (xmlSample != null)
+    {
+        foreach (var sample in xmlSample.books)
+        {
+            Console.WriteLine("id :" + sample.id + ",title : " + sample.title + ", publisher : " + sample.publisher);
+        }
+    }
+    Console.WriteLine("---------- 編集後読み取り終了 ----------");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
 
